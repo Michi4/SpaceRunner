39908bc621cbff85e597cec167e702b132b53319
Michi4 <michael.ruep@gmail.com>
2023-04-08 09:47:54 +0200

    added db and php backend

---
+++ GAME/public/login/login.php
@@ -0,0 +1,54 @@
+<?php
+// MySQL database connection parameters
+$_db_host = "localhost";
+$_db_database = "spacerunner";
+$_db_username = "root";
+$_db_password = "root";
+
+// Create a new database connection
+$conn = new mysqli($_db_host, $_db_username, $_db_password, $_db_database);
+
+// Check the database connection for errors
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Check if the form was submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the username or email and password input from the form
+    $username_email = $_POST["username-email"];
+    $password = $_POST["login-password"];
+
+    // Prepare a SQL statement to retrieve the user's password hash from the database
+    $stmt = $conn->prepare("SELECT password FROM sr_user WHERE username = ? OR email = ?");
+    $stmt->bind_param("ss", $username_email, $username_email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    // Check if a row was returned from the database
+    if ($result->num_rows == 1) {
+        // Get the password hash from the database
+        $row = $result->fetch_assoc();
+        $password_hash = $row["password"];
+
+        // Verify the password hash using the password_verify() function
+        if (password_verify($password, $password_hash)) {
+            // Update the user's last login time in the database
+            $stmt = $conn->prepare("UPDATE sr_user SET last_login = NOW() WHERE username = ? OR email = ?");
+            $stmt->bind_param("ss", $username_email, $username_email);
+            $stmt->execute();
+
+            // Redirect the user to the home page or another page
+            header("Location: home.php");
+            exit();
+        } else {
+            die("Invalid password");
+        }
+    } else {
+        die("User not found");
+    }
+}
+
+// Close the database connection
+$conn->close();
+?>
---
+++ GAME/public/login/signup.php
@@ -0,0 +1,53 @@
+<?php
+$_db_host = "localhost";
+$_db_datenbank = "spacerunner";
+$_db_username = "root";
+$_db_passwort = "root";
+
+// Establish a connection to the database
+$mysqli = new mysqli($_db_host, $_db_username, $_db_passwort, $_db_datenbank);
+
+// Check for any errors in the connection
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+}
+
+// Define variables for the form data
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash the password for security
+
+// Validate the form data using regular expressions
+if (!preg_match("/^[a-zA-Z0-9_-]{3,16}$/", $username)) {
+    die("Error: Invalid username");
+}
+
+if (!preg_match("/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/", $email)) {
+    die("Error: Invalid email address");
+}
+
+if (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/", $_POST['password'])) {
+    die("Error: Invalid password");
+}
+
+// Prepare a SQL statement to insert the data into the database
+$stmt = $mysqli->prepare("INSERT INTO sr_user (username, email, password, user_deleted, last_login) VALUES (?, ?, ?, 0, NOW())");
+
+// Bind the variables to the prepared statement as parameters
+$stmt->bind_param("sss", $username, $email, $password);
+
+// Execute the prepared statement
+$stmt->execute();
+
+// Check for any errors in the execution
+if ($stmt->errno) {
+    echo "Error: " . $stmt->error;
+} else {
+    echo "User created successfully!";
+}
+
+// Close the statement and database connection
+$stmt->close();
+$mysqli->close();
+?>
